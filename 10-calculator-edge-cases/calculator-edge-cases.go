5cd4b25692d5ba0ba99ecc1117b754ec28b20e55
janobono <janobono@gmail.com>
2024-03-27 18:55:16 +0100

    10 Calculator edge cases finish
    Signed-off-by: janobono <janobono@gmail.com>

--- 10-calculator-edge-cases/calculator-edge-cases.go
+++ 10-calculator-edge-cases/calculator-edge-cases.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"strconv"
 	"strings"
@@ -16,25 +17,49 @@ var operators = map[string]func(x, y float64) float64{
 }
 
 // parseOperand parses a string to a float64
-func parseOperand(op string) float64 {
-	parsedOp, _ := strconv.ParseFloat(op, 64)
-	return parsedOp
+func parseOperand(op string) (*float64, error) {
+	parsedOp, err := strconv.ParseFloat(op, 64)
+	if err != nil {
+		return nil, fmt.Errorf("cannot parse:%v", err)
+	}
+
+	return &parsedOp, nil
 }
 
 // calculate returns the result of a 2 operand mathematical expression
-func calculate(expr string) float64 {
-	ops := strings.Fields(expr)
-	left := parseOperand(ops[0])
-	right := parseOperand(ops[2])
-	f := operators[ops[1]]
-	result := f(left, right)
-	return result
+func calculate(expr string) (*float64, error) {
+	operands := strings.Fields(expr)
+
+	operandsCount := len(operands)
+	if operandsCount != 3 {
+		return nil, fmt.Errorf("cannot calculate: need 3 operands, got %d", operandsCount)
+	}
+
+	left, err := parseOperand(operands[0])
+	if err != nil {
+		return nil, err
+	}
+
+	right, err := parseOperand(operands[2])
+	if err != nil {
+		return nil, err
+	}
+
+	operator, ok := operators[operands[1]]
+	if !ok {
+		return nil, fmt.Errorf("cannot calculate: %s is unknown", operands[1])
+	}
+
+	result := operator(*left, *right)
+	return &result, nil
 }
 
 func main() {
-	expr := flag.String("expr", "",
-		"The expression to calculate on, separated by spaces.")
+	expr := flag.String("expr", "", "The expression to calculate on, separated by spaces.")
 	flag.Parse()
-	result := calculate(*expr)
-	log.Printf("%s = %.2f\n", *expr, result)
+	result, err := calculate(*expr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("%s = %.2f\n", *expr, *result)
 }
